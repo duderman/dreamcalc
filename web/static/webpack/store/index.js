3b9d16564c2650a802435abb09355fbe4bfdc931
Nikolay Norkin <zduderman@gmail.com>
2017-04-03 01:05:27 +0300

    dev store

--- web/static/webpack/store/index.js
+++ web/static/webpack/store/index.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore, compose } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { Iterable } from 'immutable'
 
@@ -9,21 +9,22 @@ export default function () {
   const sagaMiddleware = createSagaMiddleware()
   middlewares.push(sagaMiddleware)
 
-  const createLogger = require('redux-logger')
-  const stateTransformer = state => Iterable.isIterable(state) ? state.toJS() : state
+  if (process.env.NODE_ENV === `development`) {
+    const createLogger = require('redux-logger')
+    const stateTransformer = state => Iterable.isIterable(state) ? state.toJS() : state
 
-  const logger = createLogger({
-    stateTransformer,
-    collapsed: _ => true
-  })
+    const logger = createLogger({
+      stateTransformer,
+      collapsed: _ => true
+    })
 
-  middlewares.push(logger)
+    middlewares.push(logger)
+  }
 
-  const finalCreateStore = compose(
-    applyMiddleware(...middlewares), window.__REDUX_DEVTOOLS_EXTENSION__()
-  )(createStore)
-
-  const store = finalCreateStore(rootReducer)
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(...middlewares)
+  )
 
   sagaMiddleware.run(rootSaga)
 
