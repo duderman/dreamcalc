d110fb160531f3928ce1b2563befb3757ce75403
Nikolay Norkin <zduderman@gmail.com>
2017-04-02 15:10:50 +0300

    heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :dreamcalc, Dreamcalc.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "dreamcalc.herokuapp.com", port: 443],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -55,7 +56,12 @@ config :logger, level: :info
 #
 #     config :dreamcalc, Dreamcalc.Endpoint, server: true
 #
+config :dreamcalc, Dreamcalc.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "dreamcalc",
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 10
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+config :guardian, Guardian,
+  secret_key: System.get_env("GUARDIAN_SECRET_KEY")
+
